b96c9e408d9ad063f52ddaee9eea7c570ae0d9d2
yousuk3 <161000005+yousuk3@users.noreply.github.com>
2024-04-09 00:44:32 +0900

    fix

--- afterap.sh
+++ afterap.sh
@@ -35,19 +35,19 @@ uci set network.bat0.routing_algo='BATMAN_IV'
 uci set network.bat0.bridge_loop_avoidance='1'
 uci set network.bat0.gw_mode='off'
 uci set network.bat0.hop_penalty='30'
-#uci set network.batmesh=interface
-#uci set network.batmesh.proto='batadv_hardif'
-#uci set network.batmesh.master='bat0'
-#uci set wireless.wifinet2=wifi-iface
-#uci set wireless.wifinet2.device='radio0'
-#uci set wireless.wifinet2.mode='mesh'
-#uci set wireless.wifinet2.encryption='sae'
-#uci set wireless.wifinet2.mesh_id='mesh_an'
-#uci set wireless.wifinet2.mesh_fwding='0'
-#uci set wireless.wifinet2.mesh_rssi_threshold='0'
-#uci set wireless.wifinet2.key=${PASSWORD}
-#uci set wireless.wifinet2.network='batmesh'
-#uci add_list network.@device[0].ports='bat0.99'
+uci set network.batmesh=interface
+uci set network.batmesh.proto='batadv_hardif'
+uci set network.batmesh.master='bat0'
+uci set wireless.wifinet2=wifi-iface
+uci set wireless.wifinet2.device='radio0'
+uci set wireless.wifinet2.mode='mesh'
+uci set wireless.wifinet2.encryption='sae'
+uci set wireless.wifinet2.mesh_id='mesh_an'
+uci set wireless.wifinet2.mesh_fwding='0'
+uci set wireless.wifinet2.mesh_rssi_threshold='0'
+uci set wireless.wifinet2.key=${PASSWORD}
+uci set wireless.wifinet2.network='batmesh'
+uci add_list network.@device[0].ports='bat0.99'
 
 uci commit
 
