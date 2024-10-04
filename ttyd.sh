b77588de05371e14525c61f355ab256588ed31f8
yousuk3 <161000005+yousuk3@users.noreply.github.com>
2024-10-04 16:09:21 +0900

    ttyd fix

--- ttyd.sh
+++ ttyd.sh
@@ -3,7 +3,7 @@
 cat <<"EOF" > /usr/bin/confsoft
 #!/bin/sh
 mkdir -p /etc/config-software
-wget --no-check-certificate -O /etc/config-software/openwrt-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/openwrt-config.sh
+wget --no-check-certificate -O /etc/config-software/openwrt-config.sh https://raw.githubusercontent.com/yousuk3/config-software/custom1/openwrt-config.sh
 sh /etc/config-software/openwrt-config.sh
 EOF
 chmod +x /usr/bin/confsoft
